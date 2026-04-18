1d7c1e9c29ce714f9c2333452797b58e7bd2aec8
Keith Kwan <keith@Keiths-MacBook-Air.local>
2026-04-18 16:50:43 -0400

    adjust styling on home page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,16 +16,20 @@ export default async function Dashboard() {
 
   return (
     <div className="min-h-screen w-full h-full flex justify-center items-center ">
-      <div className="w-full h-full flex flex-col justify-center items-center gap-8">
-        <h2 className="">Welcome {session.user?.name}</h2>
-        <a href="/create-group">Create a new Group Chat</a>
-        <p>Browse through all the Group Chats Below</p>
-        <div className="w-1/2 h-full flex flex-col justify-center items-center gap-8  p-8 rounded-md shadow-md">
+      <div className="w-full h-full flex flex-col justify-center items-center gap-6">
+        <h2 className="text-3xl font-bold">Welcome {session.user?.name}</h2>
+        <a href="/create-group" className="inline-block bg-gray-900 border border-gray-600 hover:border-blue-500 hover:bg-gray-700 px-6 py-3 rounded-lg transition-all duration-200">
+            Create a new Group Chat
+        </a>
+        <p className="text-2xl font-bold">
+            Browse through all the Group Chats Below
+        </p>
+        <div className="w-1/2 h-full flex flex-col justify-center items-center gap-4 rounded-md shadow-md">
           {groups.map((group) => (
             <a
               key={group._id.toString()}
               href={`/group/${group._id.toString()}`}
-              className="bg-indigo-500 p-4 rounded-md w-2/3"
+              className="bg-gray-900 hover:bg-gray-700 text-white transition-all p-4 rounded-lg border border-gray-700 hover:border-gray-500"
             >
               {group.groupName}
             </a>
